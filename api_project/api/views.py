8d687412004e094ca8490f45a46cd9d976cc7be7
mihret <mihretmekbebe15@gmail.com>
2025-08-02 15:11:23 +0300

    api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,11 +2,14 @@ from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import IsOwner
 
-class BookList(generics.ListAPIView):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsOwner]
+
